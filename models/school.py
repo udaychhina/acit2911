2d46ad4b9fbf4c1d8979ade156ef11dfd12d59d6
Trevor L <trevorlee0323@gmail.com>
2022-05-03 10:47:12 -0700

    addition functionality added

---
+++ models/school.py
@@ -0,0 +1,25 @@
+import json
+from models.homework import Homework
+class School:
+    def __init__(self, name):
+        self.name = name
+        self.hwlist = []
+        with open("data/homework.json", 'r') as fp:
+            data = json.load(fp)
+            for homework in data:
+                self.hwlist.append(Homework(homework["course"], homework["typehw"], homework["description"], homework["duedate"]))
+
+    def __len__(self):
+        return len(self.hwlist)
+
+    def add(self, hwinstance):
+        if type(hwinstance) == Homework:
+            self.hwlist.append(hwinstance)
+
+    def save(self):    
+        holder = []
+        for i in range(len(self.hwlist)):
+            thing = self.hwlist[i].to_dict()
+            holder.append(thing)
+        with open(r"data/homework.json", 'w') as fp:
+            json.dump(holder, fp)
--- webapp.py
+++ webapp.py
@@ -1,17 +1,52 @@
 from multiprocessing.sharedctypes import Value
 from flask import Flask, render_template, request, jsonify
 import json
+from models.school import School
+from models.homework import Homework
 
 
 app = Flask(__name__)
 
+# Home page that loads the data in a table
 @app.route("/")
 def homepage():
     with open(r"data\homework.json", "r") as fp:
         data = json.load(fp)
     return render_template("home.html", homeworkdata=data), 200
 
+# Brings up the create page with the forms
+@app.route("/create")
+def testpage():
+    return render_template("create.html"), 200
 
+# Displays all the homework in a JSON format
+@app.route("/homework", methods=["GET"])
+def stupage():
+    with open(r"data\homework.json", "r") as fp:
+        data = json.load(fp)
+        return jsonify(data), 200
+
+# Homework endpoint for POST, takes course, type, description, and due date from the forms from the /create endpoint
+@app.route("/homework", methods=["POST"])
+def create_student():
+    # Getting course, type, description, and due date
+    course = request.form.get("course")
+    type = request.form.get("type")
+    desc = request.form.get("description")
+    dd = request.form.get("duedate")
+    
+    # Creating the school to place all the homework in  
+    school = School("BCIT")
+
+    try:
+        # Adding the homework to the school
+        homework = Homework(course, type, desc, dd)
+        school.add(homework)
+        school.save()
+        return "Homework added", 201
+        # If theres a value error returns 400 error
+    except ValueError:
+        return "Invalid parameters", 400
 
 
 if __name__ == "__main__":
