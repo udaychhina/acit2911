195f408965af83a9cab3ba12e16cdd49421e7bbd
Pham Minh Tan <tpham100@my.bcit.ca>
2022-05-10 12:27:28 -0700

    edit_feature

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -18,11 +18,32 @@ def test_homepage(client):
     assert b"</table>" in response.data
 
 
-def test_homework_post(client):
+def test_create_feature(client):
     response = client.post("/homework", data={
         "course": "ACIT4200", "name": "assignment 4",
         "type": "Assignment", "description": "Do some brainstorm",
         "duedate": "April 20, 2022"
+    })
+    assert response.status_code == 201
+    response = client.get("/")
+    assert b"ACIT4200" in response.data
+    assert b"Assignment" in response.data
+    assert b"Do some brainstorm" in response.data
+    assert b"April 20, 2022" in response.data
+
+    response = client.post("/homework", data={
+        "course": "", "name": "assignment 4",
+        "type": "Assignment", "description": "Do some brainstorm",
+        "duedate": "April 20, 2022"
+    })
+    assert response.status_code == 400
+
+
+def test_edit_feature(client):
+    response = client.post("/edit", data={
+        "course": "ACIT0777", "name": "assignment 7.77",
+        "type": "Assignment", "description": "Do some luck",
+        "duedate": "April 7, 2077"
     })
     assert response.status_code == 201
     response = client.get("/")
