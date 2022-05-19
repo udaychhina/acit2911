4cfb993edb6fe5907110dc9a62bed3e4d31367cb
Uday Chhina <udaychhina1337@gmail.com>
2022-05-19 14:51:38 -0700

    added a IS_HEROKU config

--- hw_tracker/__init__.py
+++ hw_tracker/__init__.py
@@ -11,6 +11,7 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'homework.sqlite'),
         TEMPLATES_AUTO_RELOAD=True,
+        IS_HEROKU=True,
     )
 
     if test_config is None:
